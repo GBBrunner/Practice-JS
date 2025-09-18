952e1009e529c2540e8ec1d88945f684a814c20b
GBBrunner <gbbrunner64@gmail.com>
2025-09-18 09:26:32 -0600

    Added Movie Ticket Eligibility

--- script.js
+++ script.js
@@ -62,16 +62,21 @@ function password() {
 // Conditionals
 
 // In-Class Exercise 3: Movie Ticket Eligibility 
-
+function checkDiscount(){
 let age = 20;
+let isStudent = true;
 // If age is less than or equal to 18 or greater than or equal to 65
 // Otherwise, print "You are not eligible for a discount."
 // If and else statements are for true or false statements
-if (age <= 18 || age >= 65) {
+if (age <= 18 || age >= 65 || isStudent==true) {
   console.log("You are elligible for a discount.");
+  alert("You are elligible for a discount.");
 } else {
-  console.log("You are not elligible for a discount.");
+  console.log("You are not elligible for a discount, Regular ticket only.");
+  alert("You are not elligible for a discount, Regular ticket only.");
 }
+}
+
 function getDashboardMessage(role) {
   let message = "Role not recognized.";
   // Switch statement is used for multiple conditions
